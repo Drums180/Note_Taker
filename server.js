fbc4d361f2950d30b1092c8a2214a84448d1168e
Drums180 <daviddomor180@hotmail.com>
2023-03-03 18:56:30 -0600

    server update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const db = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing request body
 app.use(express.json());
